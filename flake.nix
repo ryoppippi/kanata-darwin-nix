6899ab5671232f0988b05cc8d92b2e078aa4d37a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2026-01-31 18:42:21 +0000

    feat: add kanata overlay with pre-built binaries
    Add Nix flake overlay that provides pre-built Kanata binaries from
    official GitHub releases.
    
    Supported platforms:
    - x86_64-linux
    - x86_64-darwin (Intel Mac)
    - aarch64-darwin (Apple Silicon)
    
    The package downloads zip files from GitHub releases and extracts the
    appropriate binary. On macOS, the cmd_allowed variant is used to
    support command key remapping.

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Kanata keyboard remapper binaries for macOS and Linux.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          kanata = pkgs.callPackage ./default.nix { };
+          default = self.packages.${system}.kanata;
+        }
+      );
+
+      overlays.default = _final: prev: {
+        kanata = self.packages.${prev.system}.default;
+      };
+    };
+}
---
+++ package.nix
@@ -0,0 +1,85 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  unzip,
+  autoPatchelfHook,
+}:
+let
+  sourcesData = lib.importJSON ./sources.json;
+  inherit (sourcesData) version;
+  sources = sourcesData.platforms;
+
+  source =
+    sources.${stdenv.hostPlatform.system}
+    or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+
+  binaryName =
+    {
+      "x86_64-linux" = "kanata";
+      "x86_64-darwin" = "kanata_macos_cmd_allowed";
+      "aarch64-darwin" = "kanata_macos_cmd_allowed_arm64";
+    }
+    .${stdenv.hostPlatform.system};
+in
+stdenv.mkDerivation rec {
+  pname = "kanata";
+  inherit version;
+
+  src = fetchurl {
+    inherit (source) url hash;
+  };
+
+  nativeBuildInputs = [ unzip ] ++ lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
+
+  buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
+    stdenv.cc.cc.lib
+  ];
+
+  unpackPhase = ''
+    runHook preUnpack
+    unzip $src
+    runHook postUnpack
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 ${binaryName} $out/bin/kanata
+
+    runHook postInstall
+  '';
+
+  dontStrip = true;
+
+  doInstallCheck = true;
+
+  installCheckPhase = ''
+    runHook preInstallCheck
+
+    $out/bin/kanata --version
+
+    runHook postInstallCheck
+  '';
+
+  passthru = {
+    updateScript = ./update.ts;
+  };
+
+  meta = with lib; {
+    inherit version;
+    description = "Improve keyboard comfort and usability with advanced customization";
+    homepage = "https://github.com/jtroo/kanata";
+    downloadPage = "https://github.com/jtroo/kanata/releases";
+    changelog = "https://github.com/jtroo/kanata/releases/tag/v${version}";
+    license = licenses.lgpl3Only;
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    mainProgram = "kanata";
+    platforms = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    maintainers = [ ];
+  };
+}
